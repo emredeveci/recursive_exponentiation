89540252a73dfc6559db5e71cd25404cd9416ba5
Emre Deveci <23077383+emredeveci@users.noreply.github.com>
2024-02-24 16:12:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Recursive Exponentiation
+
+- This program calculates the result of an exponentiation using a recursive method
+- The base and exponent are received from the user as inputs
+## Authors
+
+- [@emredeveci](https://github.com/emredeveci)
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    static int calculatePower(int base, int exponent) {
+
+        if (exponent == 0)
+            return 1;
+        else
+            return base * calculatePower(base, exponent-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int base, exponent, result;
+        System.out.print("Please enter the base number: ");
+        base = scanner.nextInt();
+        System.out.print("Please enter the exponent(a non-negative integer): ");
+        exponent = scanner.nextInt();
+
+        System.out.println(calculatePower(base, exponent));
+    }
+}
