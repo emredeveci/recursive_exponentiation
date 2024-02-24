fd4c66f5c4237bd842004f11017b660a631862a5
Emre Deveci <23077383+emredeveci@users.noreply.github.com>
2024-02-24 16:14:39 +0300

    beautified the syntax

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int base, exponent, result;
+        int base, exponent;
         System.out.print("Please enter the base number: ");
         base = scanner.nextInt();
         System.out.print("Please enter the exponent(a non-negative integer): ");
         exponent = scanner.nextInt();
 
-        System.out.println(calculatePower(base, exponent));
+        System.out.println("The result is: " + calculatePower(base, exponent));
     }
 }
