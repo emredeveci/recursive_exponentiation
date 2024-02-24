f9e6cfbfe69b29d289ce06877f8b678d61451829
Emre Deveci <23077383+emredeveci@users.noreply.github.com>
2024-02-24 16:21:07 +0300

    added comments

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,17 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        //scanner initialization and variable definitions
         Scanner scanner = new Scanner(System.in);
         int base, exponent;
+
+        //user inputs
         System.out.print("Please enter the base number: ");
         base = scanner.nextInt();
         System.out.print("Please enter the exponent(a non-negative integer): ");
         exponent = scanner.nextInt();
 
+        //printing the result to the user calling the recursive method
         System.out.println("The result is: " + calculatePower(base, exponent));
     }
 }
